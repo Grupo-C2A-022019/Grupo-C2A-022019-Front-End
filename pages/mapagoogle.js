fe0a7411c45de6b10c1012171db567b8802cff98
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-09-21 15:33:32 -0300

    Added Google Directions example

--- pages/mapagoogle.js
+++ pages/mapagoogle.js
@@ -1,23 +1,72 @@
 import React, { Component } from "react";
-import GoogleMapReact from "google-map-react";
+import GoogleMapReact, { DirectionsRenderer } from "google-map-react";
 import getConfig from "next/config";
 
 export default function mapaGoogle() {
   return <SimpleMap />;
 }
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const Foo = ({ children }) => (
+  <div
+    style={{
+      color: "white",
+      background: "grey",
+      padding: "15px 10px",
+      display: "inline-flex",
+      textAlign: "center",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: "100%",
+      transform: "translate(-50%, -50%)"
+    }}
+  >
+    {children}
+  </div>
+);
 
 class SimpleMap extends Component {
   static defaultProps = {
     center: {
-      lat: 69.95,
-      lng: 69.33
+      lat: 41.85073,
+      lng: -87.65126
     },
-    zoom: 5
+    zoom: 15
+  };
+
+  state = {
+    directions: null
+  };
+
+  setDirections = () => {
+    // eslint-disable-next-line no-undef
+    const DirectionsService = new google.maps.DirectionsService();
+
+    DirectionsService.route(
+      {
+        // eslint-disable-next-line no-undef
+        origin: new google.maps.LatLng(41.85073, -87.65126),
+        // eslint-disable-next-line no-undef
+        destination: new google.maps.LatLng(41.85258, -87.65141),
+        // eslint-disable-next-line no-undef
+        travelMode: google.maps.TravelMode.DRIVING
+      },
+      (result, status) => {
+        // eslint-disable-next-line no-undef
+        if (status === google.maps.DirectionsStatus.OK) {
+          this.setState({
+            directions: result
+          });
+        } else {
+          console.error(`error fetching directions ${result}`);
+        }
+      }
+    );
   };
 
   render() {
+    const { directions } = this.state;
+    debugger;
+
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: "100vh", width: "100%" }}>
@@ -27,7 +76,14 @@ class SimpleMap extends Component {
           }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
-        ></GoogleMapReact>
+          onGoogleApiLoaded={this.setDirections}
+          yesIWantToUseGoogleMapApiInternals
+        >
+          <Foo lat={-34.601297} lng={-58.363833}>
+            sarasa
+          </Foo>
+          {directions && <DirectionsRenderer directions={directions} />}
+        </GoogleMapReact>
       </div>
     );
   }
