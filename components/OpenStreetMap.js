80b05e8ba128e521d3578509c722c3cc613141ee
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-09-23 20:51:18 -0300

    Refactor OpenStreetMap component

--- components/OpenStreetMap.js
+++ components/OpenStreetMap.js
@@ -1,35 +1,22 @@
-import React, { Component } from "react";
-import { Map, TileLayer, Marker } from "react-leaflet";
+import React, { useMemo } from "react";
+import { Map, TileLayer } from "react-leaflet";
 import RoutingMachine from "./RoutingMachine";
 import L from "leaflet";
 
-class MapComponent extends Component {
-  state = {
-    lat: -34.6012307,
-    lng: -58.3975367,
-    zoom: 13
-  };
-  road = [
+function OpenStreetMap() {
+  const waypoints = useMemo(() => [
     L.latLng(-34.6012307, -58.3975367),
     L.latLng(-34.6257703, -58.3923897)
-  ];
-  map = React.createRef();
-  defaultIcon = L.icon({});
-  render() {
-    const position = [this.state.lat, this.state.lng];
-    return (
-      <Map center={position} zoom={this.state.zoom} ref={this.map}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {this.road.map((point, index) => (
-          <Marker key={index} position={point} />
-        ))}
-        <RoutingMachine color="#000" map={this.map} road={this.road} />
-      </Map>
-    );
-  }
+  ]);
+
+  const bounds = useMemo(() => L.latLngBounds(...waypoints), [waypoints]);
+
+  return (
+    <Map bounds={bounds}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <RoutingMachine waypoints={waypoints} />
+    </Map>
+  );
 }
 
-export default MapComponent;
+export default OpenStreetMap;
--- components/RoutingMachine.js
+++ components/RoutingMachine.js
@@ -3,32 +3,19 @@ import L from "leaflet";
 import { withLeaflet } from "react-leaflet";
 import "leaflet-routing-machine/src";
 
+/**
+ * http://www.liedman.net/leaflet-routing-machine/api/
+ */
 class RoutingMachine extends MapLayer {
   createLeafletElement() {
-    const { color, map, road } = this.props;
+    const {
+      leaflet: { map },
+      ...props
+    } = this.props;
 
     let leafletElement = L.Routing.control({
-      waypoints: road,
-      lineOptions: {
-        styles: [
-          {
-            color,
-            opacity: 0.8,
-            weight: 6
-          }
-        ]
-      },
-      addWaypoints: false,
-      draggableWaypoints: false,
-      fitSelectedRoutes: false,
-      showAlternatives: false,
-      altLineOptions: { styles: [{ opacity: 0 }] },
-      createMarker: () => {
-        return null;
-      }
-    }).addTo(map.current.leafletElement);
-
-    leafletElement.hide(); // hide road describtion
+      ...props
+    }).addTo(map);
 
     return leafletElement.getPlan();
   }
