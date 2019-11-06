67e00511c8d7970e50e19dceb82c174a1a3cb7af
Tobias <33400774+Dova81@users.noreply.github.com>
2019-11-05 22:41:08 -0300

    add balance to the profile

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import MonetaryAmount from "./commons/MonetaryAmount";
 
-export default function Profile({ name, email, image }) {
+export default function Profile({
+  name,
+  email,
+  image,
+  balance: { amount, currency }
+}) {
   return (
     <h1>
       {name}
       {email}
       <img alt="" src={image} />
+      <div>
+        <MonetaryAmount amount={amount} currency={currency} />
+      </div>
     </h1>
   );
 }
