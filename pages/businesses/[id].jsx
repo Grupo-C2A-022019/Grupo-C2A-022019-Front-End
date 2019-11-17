62ca299a9138757b4855f37a219c65acaea6cb89
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-11-17 16:08:52 -0300

    Refactor business view

--- pages/businesses/[id].jsx
+++ pages/businesses/[id].jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { Container, Button, Grid, Paper, Typography } from "@material-ui/core";
+
 import { useRouter } from "next/router";
-import { Container, Paper, Typography } from "@material-ui/core";
 
 import ToolBar from "components/ToolBar";
+import I18n from "components/commons/I18n";
+import Image from "components/commons/Image";
 import MenuList from "components/MenuList";
 
 import useBusiness from "hooks/useBusiness";
@@ -13,25 +16,71 @@ export default function BusinessPage() {
     query: { id }
   } = useRouter();
 
-  const [business] = useBusiness(id);
   const [menus, { loading: loadingMenus }] = useBusinessMenus(id);
 
   return (
     <>
       <ToolBar />
       <Container>
-        {business && <BusinessDetails business={business} />}
+        <BusinessDetails id={id} />
         <MenuList menus={menus} variant="horizontal" loading={loadingMenus} />
       </Container>
     </>
   );
 }
 
-function BusinessDetails({ business: { name, description } }) {
+function BusinessDetails({ id }) {
+  const [business] = useBusiness(id);
+
   return (
-    <Paper>
-      <Typography>{name}</Typography>
-      <Typography>{description}</Typography>
-    </Paper>
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={9}>
+        <Paper>
+          {business && (
+            <>
+              <Typography variant="h3">{business.name}</Typography>
+              <Typography variant="h4">{business.description}</Typography>
+              <Image src={business.img} alt="business" />
+
+              <Typography variant="h5">
+                <I18n id="business.urlServ.label" />
+              </Typography>
+              <Typography>{business.urlServ}</Typography>
+              <Typography variant="h5">
+                <I18n id="business.email.label" />
+              </Typography>
+              <Typography>{business.email}</Typography>
+              <Typography variant="h5">
+                <I18n id="business.schedule.label" />
+              </Typography>
+              <Typography>{business.schedule}</Typography>
+              <Typography variant="h5">
+                <I18n id="business.telephone.label" />
+              </Typography>
+              <Typography>{business.telephone}</Typography>
+            </>
+          )}
+        </Paper>
+      </Grid>
+      <Grid item xs={12} sm={3}>
+        <Paper>
+          <PublishMenuButton businessId={id} />
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+}
+
+function PublishMenuButton({ businessId }) {
+  const { push } = useRouter();
+
+  const publishMenu = useCallback(() => {
+    push(`/menus/new?businessId=${businessId}`);
+  }, [push, businessId]);
+
+  return (
+    <Button onClick={publishMenu} variant="contained" color="primary">
+      <I18n id="business.menu.new.label" />
+    </Button>
   );
 }
