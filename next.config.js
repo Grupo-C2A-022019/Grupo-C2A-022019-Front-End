fa97a5ef8828149a9498f5c2837bdfd69669919a
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-11-24 17:27:35 -0300

    Added auth0 login

--- next.config.js
+++ next.config.js
@@ -1,22 +1,34 @@
-const path = require('path')
-require('dotenv').config()
-const withImages = require('next-images')
+const path = require("path");
+require("dotenv").config();
+const withImages = require("next-images");
 
 module.exports = withImages({
-  webpack (config, options) {
-    config.resolve.alias['components'] = path.join(__dirname, 'components')
-    config.resolve.alias['constants'] = path.join(__dirname, 'constants')
-    config.resolve.alias['contexts'] = path.join(__dirname, 'contexts')
-    config.resolve.alias['hooks'] = path.join(__dirname, 'hooks')
-    config.resolve.alias['lib'] = path.join(__dirname, 'lib')
-    config.resolve.alias['static'] = path.join(__dirname, 'static')
-    return config
+  webpack(config, options) {
+    config.resolve.alias["components"] = path.join(__dirname, "components");
+    config.resolve.alias["constants"] = path.join(__dirname, "constants");
+    config.resolve.alias["contexts"] = path.join(__dirname, "contexts");
+    config.resolve.alias["hooks"] = path.join(__dirname, "hooks");
+    config.resolve.alias["lib"] = path.join(__dirname, "lib");
+    config.resolve.alias["static"] = path.join(__dirname, "static");
+    return config;
   },
 
-  serverRuntimeConfig: { // Will only be available on the server side
+  serverRuntimeConfig: {
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_SCOPE: "openid profile",
+    REDIRECT_URI:
+      process.env.REDIRECT_URI || "http://localhost:3000/api/callback",
+    POST_LOGOUT_REDIRECT_URI:
+      process.env.POST_LOGOUT_REDIRECT_URI || "http://localhost:3000/",
+    SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
+    SESSION_COOKIE_LIFETIME: 7200 // 2 hours
   },
-  publicRuntimeConfig: { // Will be available on both server and client
-    apiUrl: process.env.NODE_ENV !== 'production' ? '/api' : process.env.API_URL,
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    apiUrl:
+      process.env.NODE_ENV !== "production" ? "/api/v1" : process.env.API_URL,
     googleMapApiKey: process.env.GOOGLE_MAP_APIKEY
   }
-})
+});
